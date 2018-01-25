8616a1629e3c03a9eaeb4dd52b0db848dc675604
paoramati <brandonmitchellpaul@gmail.com>
2018-01-25 14:04:37 +1300

    created production folder

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    'baseUrl' => '',
+    'baseUrl' => 'http://brandonpaul.x10host.com/',
     'production' => false,
     'collections' => [],
+    'contact_email' => 'brandonmitchellpaul@gmail.com',
+    'contact_phone' => '0223590779'
+
 ];
