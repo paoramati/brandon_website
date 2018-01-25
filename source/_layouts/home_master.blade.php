51d0577b0860e597256c179e25c23fd42d891677
paoramati <brandonmitchellpaul@gmail.com>
2018-01-25 17:09:04 +1300

    changed to resume template

---
+++ source/_layouts/home_master.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>@yield('title')</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Brandon Mitchell Paul">
+	<meta name="description" content="@yield('description')">
+	<meta name="keywords" content="IT, graduate, web, software, development, resume">
+
+	@yield('stylesheets')
+
+</head>
+
+@yield('content')
+
+</html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,132 +1,314 @@
-@extends('_layouts.master')
+@extends('_layouts.home_master')
 
 @section('title') 
-{{--  {{$title}}   --}}
-Brandon Paul - IT Website &amp; Blog
+  Brandon Paul's IT Resume Website
 @endsection 
 
 @section('description')
-IT resume website for Brandon Paul, NZ IT graduate
+  IT resume and website for Brandon Paul, NZ IT graduate
+@endsection
+
+@section('stylesheets')
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+    <link href="css/brandon.min.css" rel="stylesheet">
+
 @endsection
 
 @section('content')
 
-<style type="text/css">
+  <body id="page-top">
 
-</style>
-
-
-<div class="jumbotron">
-  <div class="container">
-    <h1 class="display-1">Brandon Paul</h1>
-    <p class="lead">IT Graduate, specialises in web development / OO analysis, design and development.</p>
-    <a href="docs/Brandon_Paul_CV_IT.pdf" target="_blank" class="btn btn-primary btn-lg">Download my CV</a>
-    <a href="#contact" class="btn btn-success btn-lg js-scroll-trigger">Contact Me</a>
-  </div>
-</div>
-
-<div class="container h-100">
-
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h3 class="h3">Experience</h3>
-        <p>My work and development experience:</p>
-        <ul>
-          <li>Tutoring IT w/ foreign students</li>
-          <li>Built several commercial websites</li>
-          <li>Years of corporate &amp; hospitality experience</li>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Brandon</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+          </li>
         </ul>
-        <a href="/experience" class="btn btn-success btn-lg">Visit Experience Page</a>
       </div>
+    </nav>
 
-      <div class="col">
-        <h3 class="h3">Education</h3>
-        <p>Bachelor of IT from NMIT, majoring in software development</p>
-        <ul>
-          <li>A/A+ Average(NMIT) - 6.0/7.0 GPA(UWS)</li>
-          <li>Web development</li>
-          <li>Software analysis &amp; design</li>
+    <div class="container-fluid p-0">
 
-        </ul>
-        <a href="/education" class="btn btn-success btn-lg card-link">Visit Education Page</a>
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Clarence
+            <span class="text-primary">Taylor</span>
+          </h1>
+          <div class="subheading mb-5">3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
+            <a href="mailto:name@email.com">name@email.com</a>
+          </div>
+          <p class="mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+          <ul class="list-inline list-social-icons mb-0">
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-      </div>
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experience</h2>
 
-      <div class="col">
-        <h3 class="h3">Skills</h3>
-        <p>Skilled in programming &amp; analysis/design methods</p>
-        <ul>
-          <li>HTML, CSS, JS, PHP</li>
-          <li>Java, C#</li>
-          <li>OO Analysis &amp; design techniques</li>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Senior Web Developer</h3>
+              <div class="subheading mb-3">Intelitec Solutions</div>
+              <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">March 2013 - Present</span>
+            </div>
+          </div>
 
-        </ul>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Web Developer</h3>
+              <div class="subheading mb-3">Intelitec Solutions</div>
+              <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">December 2011 - March 2013</span>
+            </div>
+          </div>
 
-        <a href="/skills" class="btn btn-success btn-lg card-link">Visit Skills Page</a>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Junior Web Designer</h3>
+              <div class="subheading mb-3">Shout! Media Productions</div>
+              <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2010 - December 2011</span>
+            </div>
+          </div>
 
-      </div>
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Web Design Intern</h3>
+              <div class="subheading mb-3">Shout! Media Productions</div>
+              <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">September 2008 - June 2010</span>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">University of Colorado Boulder</h3>
+              <div class="subheading mb-3">Bachelor of Science</div>
+              <div>Computer Science - Web Development Track</div>
+              <p>GPA: 3.23</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">August 2006 - May 2010</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">James Buchanan High School</h3>
+              <div class="subheading mb-3">Technology Magnet Program</div>
+              <p>GPA: 3.56</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">August 2002 - May 2006</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicons devicons-html5"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-css3"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-javascript"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-jquery"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-sass"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-less"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-bootstrap"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-wordpress"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-grunt"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-gulp"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-npm"></i>
+            </li>
+          </ul>
+
+          <div class="subheading mb-3">Workflow</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Mobile-First, Responsive Design</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Cross Browser Testing &amp; Debugging</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Cross Functional Teams</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Agile Development &amp; Scrum</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skiier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+          <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technolgy advancements in the front-end web development world.</p>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="projects">
+        <div class="my-auto">
+          <h2 class="mb-5">My Projects</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Google Analytics Certified Developer</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Mobile Web Specialist - Google Certification</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              1<sup>st</sup>
+              Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              1<sup>st</sup>
+              Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2<sup>nd</sup>
+              Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
+            <li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              1<sup>st</sup>
+              Place - James Buchanan High School - Hackathon 2006</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              3<sup>rd</sup>
+              Place - James Buchanan High School - Hackathon 2005</li>
+          </ul>
+        </div>
+      </section>
 
     </div>
 
-  </div>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-</div>
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-<hr/>
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+    <script src="js/brandon.min.js"></script>
 
-<!-- Contact Section -->
-<section id="contact" class="container">
-  <div class="card border-primary">
-    <div class="">
-      <h2 class="card-header card-title">Contact Me</h2>
-      <div class="card-body">
-        <h5>Email: </h5>
-
-        <a href="mailto:brandonmitchellpaul@gmail.com"><i class="fa fa-envelope-o"></i> brandonmitchellpaul@gmail.com</a>
-
-        {{--  <p>				
-          <i class="fa fa-envelope-o"></i> 
-        <script>document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'m'+'a'+'&'+'#'+'1'+'0'+'5'+';'+'l'+'t'+'&'+'#'+'1'+'1'+'1'+';'+
-        '&'+'#'+'5'+'8'+';'+'b'+'&'+'#'+'1'+'1'+'4'+';'+'%'+'6'+'1'+'n'+'d'+'o'+'n'+'%'+'&'+'#'+'5'+'4'+';'+
-        '&'+'#'+'6'+'8'+';'+'%'+'&'+'#'+'5'+'4'+';'+'9'+'&'+'#'+'3'+'7'+';'+'7'+'4'+'c'+'&'+'#'+'3'+'7'+';'+
-        '&'+'#'+'5'+'4'+';'+'&'+'#'+'5'+'6'+';'+'e'+'l'+'%'+'6'+'C'+'p'+'&'+'#'+'9'+'7'+';'+'u'+'&'+'#'+'3'+
-        '7'+';'+'&'+'#'+'5'+'4'+';'+'C'+'%'+'4'+'0'+'g'+'%'+'6'+'D'+'a'+'&'+'#'+'1'+'0'+'5'+';'+'l'+'&'+'#'+
-        '3'+'7'+';'+'&'+'
-        #'+'5'+'0'+';'+'E'+'c'+'%'+'6'+'F'+'&'+'#'+'3'+'7'+';'+'6'+'D'+"'"+'>'+'&'+'#'+'9'+
-        '8'+';'+'r'+'a'+'n'+'&'+'#'+'1'+'0'+'0'+';'+'o'+'n'+'&'+'#'+'1'+'0'+'9'+';'+'i'+'t'+'&'+'#'+'9'+'9'+
-        ';'+'h'+'e'+'l'+'l'+'&'+'#'+'1'+'1'+'2'+';'+'&'+'#'+'9'+'7'+';'+'u'+'l'+'&'+'#'+'6'+'4'+';'+'g'+'m'+
-        'a'+'i'+'&'+'#'+'1'+'0'+'8'+';'+'&'+'#'+'4'+'6'+';'+'c'+'o'+'m'+'<'+'/'+'a'+'>');</script>
-        </p>  --}}
-
-        <h5>Phone: </h5>
-        <a href="tel:+64223590779"><i class="glyphicon glyphicon-envelope"></i> 0223 590 779</a>
-
-      </div>
-    </div>
-  </div>
-</section>
-
-<button class="btn btn-danger" data-toggle="modal" data-target="#pop">
-  View Popup
-</button>
-     
-<!-- Modal -->
-<div class="modal fade" id="pop" tabindex="-1" role="dialog" aria-labelledby="pop-label" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title" id="pop-label">Popup!</h4>
-      </div>
-      <div class="modal-body">
-        A "Modal" is a Bootstrap popup window that uses Javascript.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+  </body>
 
 @endsection
-
